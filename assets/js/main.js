80eafb2c5358b01abadc7f2efd1aededf7d41774
vadamk <v.kravchuk@aol.com>
2017-02-11 16:42:38 +0200

    work version 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,11 @@ var navbarHeight = 50;
 $(function() {
 
 	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
-	
+});
+
+$(document).ready(function() {
+			// $("#ModalThx").modal('show');
+	// CostCalc();
 });
 
 $("#ModalForm form").submit(function() { //Change
@@ -12,18 +16,59 @@ $("#ModalForm form").submit(function() { //Change
 
 	if (vals[0].value != "" && vals[1].value != "" && vals[2].value != "") {
 	
-		console.log(vals)
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize(),
+		}).done(function() {
+			th[0].reset();
+			$("#ModalForm").modal('hide');
+			$("#ModalThx").modal('show');
+		});
+	} else {
+		alert("Будь ласка заповніть обов'язкові поля.")
+	}
+
+  return false;
+});
+
+$("#FooterForm form").submit(function() { //Change
+	var th = $(this);
+	var vals = th.serializeArray();
+
+	if (vals[0].value != "") {
 
 		$.ajax({
 			type: "POST",
 			url: "mail.php", //Change
 			data: th.serialize(),
 		}).done(function() {
-			alert("Thank you!\nWe will definitely contact you in the near future.");
+			th[0].reset();
+			$("#ModalThx").modal('show');
 		});
 	} else {
-		$("#ModalForm").modal('hide');
-		alert("Name and e-mail is a required field!")
+		alert("Будь ласка заповніть обов'язкові поля.")
+	}
+
+  return false;
+});
+
+$("#TryForm form").submit(function() { //Change
+	var th = $(this);
+	var vals = th.serializeArray();
+
+	if (vals[0].value != "" && vals[1].value != "") {
+
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize(),
+		}).done(function() {
+			th[0].reset();
+			$("#ModalThx").modal('show');
+		});
+	} else {
+		alert("Будь ласка заповніть обов'язкові поля.")
 	}
 
   return false;
@@ -36,6 +81,16 @@ $('.faq-list li .collapse').on('show.bs.collapse', function () {
 $('.faq-list li .collapse').on('hide.bs.collapse', function () {
   $('.faq-list li a[href=#'+$(this).attr("id")+'] .close-icon').removeClass('open')
 })
+
+// $("#costMonth").keyup(CostCalc);
+
+// function CostCalc() {
+// 	var costMonth = document.getElementById("costMonth").value;
+// 	var costHour = Math.round(parseInt(costMonth, 10)/22/8);
+// 	var costWeek = Math.round(costHour*8)+"-"+Math.round(costHour*10);
+// 	document.getElementById('costHour').innerHTML = costHour;
+// 	document.getElementById('costWeek').innerHTML = costWeek;
+// };
 
 function changeFormTypeValue(v) {
 	document.getElementById('ModalFormType').value = v;
